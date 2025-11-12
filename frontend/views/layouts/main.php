edd37fc2188d6006018f41e22601a681fb216a40
john ramirez <jonnywells2020@gmail.com>
2025-11-12 19:13:17 -0400

    google-ads-manager

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -206,16 +206,6 @@ $this->params['analytics_config'] = $analytics_config;
     <!-- End Google Tag Manager -->
     <?php endif; ?>
 
-    <?php if (!empty($customScripts)): ?>
-    <!-- Custom Scripts -->
-    <?php foreach ($customScripts as $script): ?>
-    <!-- Custom Script: <?php echo Html::encode($script->label); ?> -->
-    <?php echo $script->script; ?>
-    <!-- End Custom Script: <?php echo Html::encode($script->label); ?> -->
-    <?php endforeach; ?>
-    <!-- End Custom Scripts -->
-    <?php endif; ?>
-
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -316,6 +306,16 @@ $this->params['analytics_config'] = $analytics_config;
             }
         });
     </script>
+
+    <?php if (!empty($customScripts)): ?>
+    <!-- Custom Scripts -->
+    <?php foreach ($customScripts as $script): ?>
+    <!-- Custom Script: <?php echo Html::encode($script->label); ?> -->
+    <?php echo $script->script; ?>
+    <!-- End Custom Script: <?php echo Html::encode($script->label); ?> -->
+    <?php endforeach; ?>
+    <!-- End Custom Scripts -->
+    <?php endif; ?>
 
     <?php $this->endBody() ?>
 </body>
